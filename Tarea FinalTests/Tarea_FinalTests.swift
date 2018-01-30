a856f88b6b2f92220b65cdab96d91354a132c0ac
Frederick Munoz <frederickmunoz@fredericks-macbook-pro.local>
2018-01-29 18:19:33 -0600

    Initial Commit

---
+++ Tarea FinalTests/Tarea_FinalTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tarea_FinalTests.swift
+//  Tarea FinalTests
+//
+//  Created by Frederick Munoz on 1/29/18.
+//  Copyright © 2018 Frederick Munoz. All rights reserved.
+//
+
+import XCTest
+@testable import Tarea_Final
+
+class Tarea_FinalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
